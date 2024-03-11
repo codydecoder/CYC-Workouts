7888f5f108dae73c837ceef912f5eeea85e3df69
whoever11 <cjthesedays000@gmail.com>
2024-03-11 14:35:22 -0400

    homepage

---
+++ CYCworkouts/src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Homepage = ({ user }) => {
+  const navigate = useNavigate()
+
+  return (
+    <div className="homepage-container">
+      <h1>Welcome to the Fitness Tracker App!</h1>
+      {!user ? (
+        <>
+        </>
+      ) : (
+        <>
+          <div className="user-info">
+            <h2>User Information</h2>
+            <p>Name: {user.name}</p>
+          </div>
+          <button onClick={() => navigate('/create-exercise')}>Create Exercises</button>
+          <button onClick={() => navigate('/create-workout-plan')}>Create Workout Plan</button>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default Homepage
