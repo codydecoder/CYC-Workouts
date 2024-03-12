6623d5f8c6f8b807ca5bf578498f2f96e8051715
codydecoder <142354876+codydecoder@users.noreply.github.com>
2024-03-12 08:51:55 -0500

    Server complete

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express') //express
+const logger = require('morgan') //morgan
+const bodyParser = require('body-parser')
+const db = require('./db') //db
+const cors = require('cors') //cors
+
+const exerciseController = require('./controllers/exerciseController')
+const workoutPlanController = require('./controllers/workoutPlanController')
+const userController = require('./controllers/userController')
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+
+
+
+app.get('/', (req, res) => res.send('This is our landing page!'))
+//exercise controllers
+app.get('/exercises', exerciseController.getAllExercises)
+app.get('/exercises/:id', exerciseController.getExerciseById)
+app.post('/exercises', exerciseController.createExercise)
+app.put('/exercises/:id', exerciseController.updateExercise)
+app.delete('/exercises/:id', exerciseController.deleteExercise)
+//workoutPlan controllers
+app.get('/workoutPlans', workoutPlanController.getAllWorkoutPlans)
+app.get('/workoutPlans/:id', workoutPlanController.getWorkoutPlanById)
+app.post('/workoutPlans', workoutPlanController.createWorkoutPlan)
+app.put('/workoutPlans/:id', workoutPlanController.updateWorkoutPlan)
+app.delete('/workoutPlans/:id', workoutPlanController.deleteWorkoutPlan)
+//user controllers
+app.get('/users', userController.getAllUsers)
+app.get('/users/:id', userController.getUserById)
+app.post('/users', userController.createUser)
+app.put('/users/:id', userController.updateUser)
+app.delete('/users/:id', userController.deleteUser)
+
